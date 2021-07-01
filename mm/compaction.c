074faac28bab77b9300d81b1ba2efceea27fb1ec
Liu Xiang <liu.xiang@zlingsmart.com>
2021-06-30 18:50:51 -0700

    mm: compaction: remove duplicate !list_empty(&sublist) check
    The list_splice_tail(&sublist, freelist) also do !list_empty(&sublist)
    check, so remove the duplicate call.
    
    Link: https://lkml.kernel.org/r/20210609095409.19920-1-liu.xiang@zlingsmart.com
    Signed-off-by: Liu Xiang <liu.xiang@zlingsmart.com>
    Reviewed-by: David Hildenbrand <david@redhat.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: DennySPb <dennyspb@gmail.com>

--- mm/compaction.c
+++ mm/compaction.c
@@ -1211,8 +1211,7 @@ move_freelist_head(struct list_head *freelist, struct page *freepage)
 
 	if (!list_is_last(freelist, &freepage->lru)) {
 		list_cut_before(&sublist, freelist, &freepage->lru);
-		if (!list_empty(&sublist))
-			list_splice_tail(&sublist, freelist);
+		list_splice_tail(&sublist, freelist);
 	}
 }
 
@@ -1229,8 +1228,7 @@ move_freelist_tail(struct list_head *freelist, struct page *freepage)
 
 	if (!list_is_first(freelist, &freepage->lru)) {
 		list_cut_position(&sublist, freelist, &freepage->lru);
-		if (!list_empty(&sublist))
-			list_splice_tail(&sublist, freelist);
+		list_splice_tail(&sublist, freelist);
 	}
 }
 
