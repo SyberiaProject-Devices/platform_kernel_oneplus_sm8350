978eadc91748e350a092c4cdf8738832a6de535b
Rick Yiu <rickyiu@google.com>
2022-01-07 18:31:06 +0800

    ANDROID: sched: Add vendor hook for util-update related functions
    Vendor may have the need to implement their own util tracking.
    
    Bug: 201260585
    Signed-off-by: Rick Yiu <rickyiu@google.com>
    Change-Id: I973902e6ff82a85ecd029ac5a78692d629df1ebe
    Signed-off-by: DennySPb <dennyspb@gmail.com>

--- kernel/sched/pelt.c
+++ kernel/sched/pelt.c
@@ -32,6 +32,7 @@
 
 int pelt_load_avg_period = PELT32_LOAD_AVG_PERIOD;
 int pelt_load_avg_max = PELT32_LOAD_AVG_MAX;
+EXPORT_SYMBOL_GPL(pelt_load_avg_max);
 const u32 *pelt_runnable_avg_yN_inv = pelt32_runnable_avg_yN_inv;
 
 int get_pelt_halflife(void)
@@ -241,9 +242,8 @@ accumulate_sum(u64 delta, struct sched_avg *sa,
  *   load_avg = u_0` + y*(u_0 + u_1*y + u_2*y^2 + ... )
  *            = u_0 + u_1*y + u_2*y^2 + ... [re-labeling u_i --> u_{i+1}]
  */
-static __always_inline int
-___update_load_sum(u64 now, struct sched_avg *sa,
-		  unsigned long load, unsigned long runnable, int running)
+int ___update_load_sum(u64 now, struct sched_avg *sa,
+		       unsigned long load, unsigned long runnable, int running)
 {
 	u64 delta;
 
@@ -293,6 +293,7 @@ ___update_load_sum(u64 now, struct sched_avg *sa,
 
 	return 1;
 }
+EXPORT_SYMBOL_GPL(___update_load_sum);
 
 /*
  * When syncing *_avg with *_sum, we must take into account the current
@@ -318,8 +319,7 @@ ___update_load_sum(u64 now, struct sched_avg *sa,
  * the period_contrib of cfs_rq when updating the sched_avg of a sched_entity
  * if it's more convenient.
  */
-static __always_inline void
-___update_load_avg(struct sched_avg *sa, unsigned long load)
+void ___update_load_avg(struct sched_avg *sa, unsigned long load)
 {
 	u32 divider = get_pelt_divider(sa);
 
@@ -330,6 +330,7 @@ ___update_load_avg(struct sched_avg *sa, unsigned long load)
 	sa->runnable_avg = div_u64(sa->runnable_sum, divider);
 	WRITE_ONCE(sa->util_avg, sa->util_sum / divider);
 }
+EXPORT_SYMBOL_GPL(___update_load_avg);
 
 /*
  * sched_entity:
