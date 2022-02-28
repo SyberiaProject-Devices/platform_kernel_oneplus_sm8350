209e7fd270ff9333367a31190a9675897e1efeb8
Tengfei Fan <quic_tengfan@quicinc.com>
2022-02-28 14:09:10 +0800

    sched/walt: don't requeue mvp task if only have one mvp task on rq
    Don't requeue mvp task if only have one mvp task on rq, because
    only have one mvp task on rq, current mvp task will be select for
    running if this mvp task runtime still less than limit.
    
    Change-Id: Idaf343e7471665c01fe711e86a3d7747e501ed76
    Signed-off-by: Tengfei Fan <quic_tengfan@quicinc.com>
    Signed-off-by: DennySPb <dennyspb@gmail.com>

--- kernel/sched/walt/walt.c
+++ kernel/sched/walt/walt.c
@@ -4093,6 +4093,9 @@ static void walt_cfs_account_mvp_runtime(struct rq *rq, struct task_struct *curr
 		return;
 	}
 
+	if (rq->wrq.num_mvp_tasks == 1)
+		return;
+
 	/* slice expired. re-queue the task */
 	list_del(&curr->wts.mvp_list);
 	rq->wrq.num_mvp_tasks--;
