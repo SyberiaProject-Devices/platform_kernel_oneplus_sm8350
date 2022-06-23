e017c83f1f826ae549f44d03ac8fa2551804284e
chungkai <chungkai@google.com>
2022-06-23 05:00:24 +0000

    vendor_hook: sched: inherit binder's uclamp and prefer_idle
    We may still have long binder transaction due to insufficient uclamp, so expand binder_prioity to inherit uclamp and prefer_idle as well.
    
    To avoid nested problem, only accept the first inheritance when we got subsequent transactions from the same target.
    
    Test: work as expected
    Bug: 226003124
    Signed-off-by: chungkai <chungkai@google.com>
    Change-Id: I23a7b213794374200eb58dbf371d6842c1fd28e1
    Signed-off-by: DennySPb <dennyspb@gmail.com>

--- kernel/sched/vendor_sched/init.c
+++ kernel/sched/vendor_sched/init.c
@@ -11,11 +11,13 @@
 #include <linux/module.h>
 #include <trace/hooks/cgroup.h>
 #include <trace/hooks/fs.h>
+#include <trace/hooks/binder.h>
 #include <trace/hooks/sched.h>
 #include <trace/hooks/topology.h>
 #include <trace/hooks/cpufreq.h>
 
 #include "sched_priv.h"
+#include "../../drivers/android/binder_internal.h"
 
 extern void init_uclamp_stats(void);
 extern int create_procfs_node(void);
@@ -56,6 +58,11 @@ extern void rvh_update_rt_rq_load_avg_pixel_mod(void *data, u64 now, struct rq *
 extern void rvh_set_task_cpu_pixel_mod(void *data, struct task_struct *p, unsigned int new_cpu);
 extern void rvh_enqueue_task_pixel_mod(void *data, struct rq *rq, struct task_struct *p, int flags);
 extern void rvh_dequeue_task_pixel_mod(void *data, struct rq *rq, struct task_struct *p, int flags);
+
+extern void vh_binder_set_priority_pixel_mod(void *data, struct binder_transaction *t,
+	struct task_struct *task);
+extern void vh_binder_restore_priority_pixel_mod(void *data, struct binder_transaction *t,
+	struct task_struct *task);
 
 extern void android_vh_show_max_freq(void *unused, struct cpufreq_policy *policy,
 						unsigned int *max_freq);
@@ -200,6 +207,16 @@ static int vh_sched_init(void)
 		return ret;
 
 	ret = register_trace_android_vh_sched_setaffinity_early(vh_sched_setaffinity_mod, NULL);
+	if (ret)
+		return ret;
+
+	ret = register_trace_android_vh_binder_set_priority(
+		vh_binder_set_priority_pixel_mod, NULL);
+	if (ret)
+		return ret;
+
+	ret = register_trace_android_vh_binder_restore_priority(
+		vh_binder_restore_priority_pixel_mod, NULL);
 	if (ret)
 		return ret;
 
