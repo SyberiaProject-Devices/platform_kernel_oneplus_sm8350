97cc93a3007663c0d990103c5194f6743ca2545d
Juhyung Park <qkrwngud825@gmail.com>
2022-07-20 18:37:39 +0900

    oplus_chg: disable oplus_chg_print_log()
    Spams the log too much
    
    Signed-off-by: Juhyung Park <qkrwngud825@gmail.com>
    Signed-off-by: DennySPb <dennyspb@gmail.com>

--- drivers/power/oplus_chg/oplus_charger.c
+++ drivers/power/oplus_chg/oplus_charger.c
@@ -8695,6 +8695,7 @@ static void oplus_chg_kpoc_power_off_check(struct oplus_chg_chip *chip)
 
 static void oplus_chg_print_log(struct oplus_chg_chip *chip)
 {
+#if 0
 	if (chip->vbatt_num == 1) {
 		charger_xlog_printk(
 			CHG_LOG_CRTI,
@@ -8753,6 +8754,7 @@ static void oplus_chg_print_log(struct oplus_chg_chip *chip)
 	if (chip->charger_type == POWER_SUPPLY_TYPE_USB_DCP) {
 		oplus_warp_print_log();
 	}
+#endif
 #endif
 }
 
