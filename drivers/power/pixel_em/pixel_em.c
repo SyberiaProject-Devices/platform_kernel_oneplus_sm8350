kerneltoast/android_kernel_google_gs201

drivers: thermal: Using PDs from pixel-em.
Use the pixel-em driver's CPU PDs if available.

Bug: 170647767
Change-Id: If48924ad433e83f4b6d0366690fb11b5a047ee33
Signed-off-by: Vincent Palomares <paillon@google.com>

drivers/soc/google/pixel_em/pixel_em.c
@@ -24,6 +24,10 @@
 extern struct em_perf_domain **vendor_sched_cpu_to_em_pd;
 #endif
 
+#if IS_ENABLED(CONFIG_EXYNOS_CPU_THERMAL)
+extern struct em_perf_domain **exynos_cpu_cooling_cpu_to_em_pd;
+#endif
+
 static int pixel_em_max_cpu;
 static int pixel_em_num_cpu_pds;
 
@@ -429,6 +433,11 @@ static int pixel_em_drv_probe(struct platform_device *dev)
 #if IS_ENABLED(CONFIG_VH_SCHED)
 	pr_info("Publishing PDs to vh_sched!\n");
 	WRITE_ONCE(vendor_sched_cpu_to_em_pd, cpu_to_em_pd);
+#endif
+
+#if IS_ENABLED(CONFIG_EXYNOS_CPU_THERMAL)
+	pr_info("Publishing PDs to exynos_cpu_cooling!\n");
+	WRITE_ONCE(exynos_cpu_cooling_cpu_to_em_pd, cpu_to_em_pd);
 #endif
 
 	return res;

SyberiaProject-Devices/platform_kernel_oneplus_sm8350

(cherry picked from commit c47e49fc5b4d4d556b0b78fe0802ed67d64bc6b8)
Change-Id: I0b587ff89e2e2877e4919cfaa2e445cf87c6be83
Signed-off-by: DennySPb <dennyspb@gmail.com>

drivers/power/pixel_em/pixel_em.c
@@ -24,6 +24,10 @@
 extern struct em_perf_domain **vendor_sched_cpu_to_em_pd;
 #endif
 
+#if IS_ENABLED(CONFIG_CPU_THERMAL)
+extern struct em_perf_domain **pixel_cpu_cooling_cpu_to_em_pd;
+#endif
+
 static int pixel_em_max_cpu;
 static int pixel_em_num_cpu_pds;
 
@@ -429,6 +433,11 @@ static int pixel_em_drv_probe(struct platform_device *dev)
 #if IS_ENABLED(CONFIG_VENDOR_SCHED)
 	pr_info("Publishing PDs to vh_sched!\n");
 	WRITE_ONCE(vendor_sched_cpu_to_em_pd, cpu_to_em_pd);
+#endif
+
+#if IS_ENABLED(CONFIG_CPU_THERMAL)
+	pr_info("Publishing PDs to pixel_cpu_cooling!\n");
+	WRITE_ONCE(pixel_cpu_cooling_cpu_to_em_pd, cpu_to_em_pd);
 #endif
 
 	return res;