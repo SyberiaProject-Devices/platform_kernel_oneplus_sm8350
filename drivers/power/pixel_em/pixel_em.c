kerneltoast/android_kernel_google_gs201

vendor_hook: sched: Use PDs from pixel-em
Use the pixel-em driver's CPU PDs if available.

Bug: 170647767
Change-Id: I54a251d0ce4764f72778c06291d4cc76d550463e
Signed-off-by: Vincent Palomares <paillon@google.com>

drivers/soc/google/pixel_em/pixel_em.c
@@ -20,6 +20,10 @@
 #include <linux/slab.h>
 #include <linux/string.h>
 
+#if IS_ENABLED(CONFIG_VH_SCHED)
+extern struct em_perf_domain **vendor_sched_cpu_to_em_pd;
+#endif
+
 static int pixel_em_max_cpu;
 static int pixel_em_num_cpu_pds;
 
@@ -422,6 +426,10 @@ static int pixel_em_drv_probe(struct platform_device *dev)
 	platform_dev = dev;
 
 	// Register EM table to all needed drivers here.
+#if IS_ENABLED(CONFIG_VH_SCHED)
+	pr_info("Publishing PDs to vh_sched!\n");
+	WRITE_ONCE(vendor_sched_cpu_to_em_pd, cpu_to_em_pd);
+#endif
 
 	return res;
 }

SyberiaProject-Devices/platform_kernel_oneplus_sm8350

(cherry picked from commit 8bcc32030e62101791ba9eff2d5136916a075b34)
Change-Id: I5fce226dd85b1fcf0fec8d3fb44c8ef927979b32
Signed-off-by: DennySPb <dennyspb@gmail.com>

drivers/power/pixel_em/pixel_em.c
@@ -20,6 +20,10 @@
 #include <linux/slab.h>
 #include <linux/string.h>
 
+#if IS_ENABLED(CONFIG_VENDOR_SCHED)
+extern struct em_perf_domain **vendor_sched_cpu_to_em_pd;
+#endif
+
 static int pixel_em_max_cpu;
 static int pixel_em_num_cpu_pds;
 
@@ -422,6 +426,10 @@ static int pixel_em_drv_probe(struct platform_device *dev)
 	platform_dev = dev;
 
 	// Register EM table to all needed drivers here.
+#if IS_ENABLED(CONFIG_VENDOR_SCHED)
+	pr_info("Publishing PDs to vh_sched!\n");
+	WRITE_ONCE(vendor_sched_cpu_to_em_pd, cpu_to_em_pd);
+#endif
 
 	return res;
 }