MiCode/Xiaomi_Kernel_OpenSource

sched/walt/avg: cache window based statistics
Previously it was possible to update core control with new
statistics and those statistics would be unavailable to
any other decisions that need to be made.

Update sched_avg to cache the statistics locally and simply
pass a reference to that information back to core control.
Provide a cluster util function to provide window based load.
Create a sysctl node and threshold to use to compare against
cluster util.

Change-Id: I8cc40603e49e29e7f8a343f6ff667ce9abe09cb5
Signed-off-by: Stephen Dickey <quic_dickey@quicinc.com>

kernel/sched/walt/sched_avg.c
@@ -34,6 +34,28 @@ static DEFINE_PER_CPU(u64, util_hyst_time);
 #define NR_THRESHOLD_PCT		40
 #define MAX_RTGB_TIME (sysctl_sched_coloc_busy_hyst_max_ms * NSEC_PER_MSEC)
 
+struct sched_avg_stats stats[WALT_NR_CPUS];
+unsigned int cstats_util_pct[MAX_CLUSTERS];
+
+/**
+ * sched_get_cluster_util_pct
+ * @return: provide the percentage of this cluter that was used in the
+ *          previous window.
+ *
+ * This routine may be called any number of times as needed during
+ * a window, but will always return the same result until window
+ * rollover.
+ */
+unsigned int sched_get_cluster_util_pct(struct walt_sched_cluster *cluster)
+{
+	unsigned int cluster_util_pct = 0;
+
+	if (cluster->id < MAX_CLUSTERS)
+		cluster_util_pct = cstats_util_pct[cluster->id];
+
+	return cluster_util_pct;
+}
+
 /**
  * sched_get_nr_running_avg
  * @return: Average nr_running, iowait and nr_big_tasks value since last poll.
@@ -41,18 +63,22 @@ static DEFINE_PER_CPU(u64, util_hyst_time);
  *	    of accuracy.
  *
  * Obtains the average nr_running value since the last poll.
- * This function may not be called concurrently with itself
+ * This function may not be called concurrently with itself.
+ *
+ * It is assumed that this function is called at most once per window
+ * rollover.
  */
-void sched_get_nr_running_avg(struct sched_avg_stats *stats)
+struct sched_avg_stats *sched_get_nr_running_avg(void)
 {
 	int cpu;
 	u64 curr_time = sched_clock();
 	u64 period = curr_time - last_get_time;
 	u64 tmp_nr, tmp_misfit;
 	bool any_hyst_time = false;
+	struct walt_sched_cluster *cluster;
 
 	if (!period)
-		return;
+		goto done;
 
 	/* read and reset nr_running counts */
 	for_each_possible_cpu(cpu) {
@@ -96,6 +122,27 @@ void sched_get_nr_running_avg(struct sched_avg_stats *stats)
 		spin_unlock_irqrestore(&per_cpu(nr_lock, cpu), flags);
 	}
 
+	/* collect cluster load stats */
+	for_each_sched_cluster(cluster) {
+		unsigned int num_cpus = cpumask_weight(&cluster->cpus);
+		unsigned int sum_util_pct = 0;
+
+		/* load is already scaled, see freq_policy_load/prev_runnable_sum */
+		for_each_cpu(cpu, &cluster->cpus) {
+			struct rq *rq = cpu_rq(cpu);
+			struct walt_rq *wrq = (struct walt_rq *) rq->android_vendor_data1;
+
+			/* compute the % this cpu's utilization of the cpu capacity,
+			 * and sum it across all cpus
+			 */
+			sum_util_pct +=
+				(wrq->util * 100) / arch_scale_cpu_capacity(cpu);
+		}
+
+		/* calculate the averge per-cpu utilization */
+		cstats_util_pct[cluster->id] = sum_util_pct / num_cpus;
+	}
+
 	for_each_possible_cpu(cpu) {
 		if (per_cpu(coloc_hyst_time, cpu)) {
 			any_hyst_time = true;
@@ -107,6 +154,8 @@ void sched_get_nr_running_avg(struct sched_avg_stats *stats)
 
 	last_get_time = curr_time;
 
+done:
+	return &stats[0];
 }
 EXPORT_SYMBOL(sched_get_nr_running_avg);
 

SyberiaProject-Devices/platform_kernel_oneplus_sm8350

Signed-off-by: DennySPb <dennyspb@gmail.com>

kernel/sched/walt/sched_avg.c
@@ -46,6 +46,28 @@ static DEFINE_PER_CPU(u64, util_hyst_time);
 #define NR_THRESHOLD_PCT		40
 #define MAX_RTGB_TIME (sysctl_sched_coloc_busy_hyst_max_ms * NSEC_PER_MSEC)
 
+struct sched_avg_stats stats[NR_CPUS];
+unsigned int cstats_util_pct[MAX_CLUSTERS];
+
+/**
+ * sched_get_cluster_util_pct
+ * @return: provide the percentage of this cluter that was used in the
+ *          previous window.
+ *
+ * This routine may be called any number of times as needed during
+ * a window, but will always return the same result until window
+ * rollover.
+ */
+unsigned int sched_get_cluster_util_pct(struct walt_sched_cluster *cluster)
+{
+	unsigned int cluster_util_pct = 0;
+
+	if (cluster->id < MAX_CLUSTERS)
+		cluster_util_pct = cstats_util_pct[cluster->id];
+
+	return cluster_util_pct;
+}
+
 /**
  * sched_get_nr_running_avg
  * @return: Average nr_running, iowait and nr_big_tasks value since last poll.
@@ -53,18 +75,22 @@ static DEFINE_PER_CPU(u64, util_hyst_time);
  *	    of accuracy.
  *
  * Obtains the average nr_running value since the last poll.
- * This function may not be called concurrently with itself
+ * This function may not be called concurrently with itself.
+ *
+ * It is assumed that this function is called at most once per window
+ * rollover.
  */
-void sched_get_nr_running_avg(struct sched_avg_stats *stats)
+struct sched_avg_stats *sched_get_nr_running_avg(void)
 {
 	int cpu;
 	u64 curr_time = sched_clock();
 	u64 period = curr_time - last_get_time;
 	u64 tmp_nr, tmp_misfit;
 	bool any_hyst_time = false;
+	struct walt_sched_cluster *cluster;
 
 	if (!period)
-		return;
+		goto done;
 
 	/* read and reset nr_running counts */
 	for_each_possible_cpu(cpu) {
@@ -108,6 +134,26 @@ void sched_get_nr_running_avg(struct sched_avg_stats *stats)
 		spin_unlock_irqrestore(&per_cpu(nr_lock, cpu), flags);
 	}
 
+	/* collect cluster load stats */
+	for_each_sched_cluster(cluster) {
+		unsigned int num_cpus = cpumask_weight(&cluster->cpus);
+		unsigned int sum_util_pct = 0;
+
+		/* load is already scaled, see freq_policy_load/prev_runnable_sum */
+		for_each_cpu(cpu, &cluster->cpus) {
+			struct rq *rq = cpu_rq(cpu);
+
+			/* compute the % this cpu's utilization of the cpu capacity,
+			 * and sum it across all cpus
+			 */
+			sum_util_pct +=
+				(rq->wrq.util * 100) / arch_scale_cpu_capacity(cpu);
+		}
+
+		/* calculate the averge per-cpu utilization */
+		cstats_util_pct[cluster->id] = sum_util_pct / num_cpus;
+	}
+
 	for_each_possible_cpu(cpu) {
 		if (per_cpu(coloc_hyst_time, cpu)) {
 			any_hyst_time = true;
@@ -119,6 +165,8 @@ void sched_get_nr_running_avg(struct sched_avg_stats *stats)
 
 	last_get_time = curr_time;
 
+done:
+	return &stats[0];
 }
 EXPORT_SYMBOL(sched_get_nr_running_avg);
 