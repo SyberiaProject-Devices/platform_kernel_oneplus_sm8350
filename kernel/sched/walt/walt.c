95d10e920df686dc489633171a0da4467a70ed5e
DennySPb <dennyspb@gmail.com>
2023-04-24 16:10:43 +0300

    Revert "sched/walt: Fix lock_cpus mask setting during IC migrations"
    This reverts commit 0070560654ce9d530f807ae1dc7f7ae62ae6fb4e.
    
    Signed-off-by: DennySPb <dennyspb@gmail.com>

--- kernel/sched/walt/walt.c
+++ kernel/sched/walt/walt.c
@@ -3653,17 +3653,14 @@ static inline void irq_work_restrict_to_mig_clusters(cpumask_t *lock_cpus)
 	int cpu;
 
 	for_each_sched_cluster(cluster) {
-		bool keep = false;
 		for_each_cpu(cpu, &cluster->cpus) {
 			rq = cpu_rq(cpu);
 			/* remove this cluster if it's not being notified */
 			if (!rq->wrq.notif_pending) {
-				keep = true;
+				cpumask_andnot(lock_cpus, lock_cpus, &cluster->cpus);
 				break;
 			}
 		}
-		if (!keep)
-			cpumask_andnot(lock_cpus, lock_cpus, &cluster->cpus);
 	}
 }
 
